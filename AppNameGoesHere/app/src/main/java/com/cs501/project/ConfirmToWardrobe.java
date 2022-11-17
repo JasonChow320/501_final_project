92dbf7870a8d25fe445e66f2ec5f1640588a278a
Michael Matta <mjmatta@bu.edu>
2022-11-16 20:45:51 -0500

    some placeholder buttons for now but very good skeleton for what we're gonna have in ConfirmToWardrobe

--- AppNameGoesHere/app/src/main/java/com/cs501/project/ConfirmToWardrobe.java
+++ AppNameGoesHere/app/src/main/java/com/cs501/project/ConfirmToWardrobe.java
@@ -2,9 +2,12 @@ package com.cs501.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
@@ -19,11 +22,32 @@ public class ConfirmToWardrobe extends AppCompatActivity {
         setContentView(R.layout.confirm_to_wardrobe);
 
         ImageView editItemImage = (ImageView) findViewById(R.id.editItemImage);
+        Button confirm = (Button) findViewById(R.id.ConfirmAdd);
 
         ArrayList<String> fileNames = getIntent().getStringArrayListExtra("fileNames");
         System.out.println(fileNames.size() + " submitted");
 
         Bitmap b = BitmapFactory.decodeFile(fileNames.get(0));
         editItemImage.setImageBitmap(b);
+        System.out.println(b.getHeight() + " x " + b.getWidth());
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //1. Add submission to database
+                //2. Remove 1st item of arraylist and refresh activity with new list
+                //3. At end of list return to menu.
+                //Question: How can we reuse this Activity for editing clothes items?
+                fileNames.remove(0);
+                if(fileNames.size() > 0) {
+                    Intent i = new Intent(ConfirmToWardrobe.this, ConfirmToWardrobe.class);
+                    i.putExtra("fileNames", fileNames);
+                    finish();
+                    startActivity(i);
+                } else {
+                    finish();
+                }
+            }
+        });
     }
 }
