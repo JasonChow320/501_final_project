596a39d24a54763d00d825a9bf591274e3caafc3
Jason Chow <jchow320@bu.edu>
2022-12-04 16:29:40 -0500

    save changes

--- AppNameGoesHere/app/src/main/java/com/cs501/project/GenerateOutfit.java
+++ AppNameGoesHere/app/src/main/java/com/cs501/project/GenerateOutfit.java
@@ -161,12 +161,10 @@ public class GenerateOutfit extends AppCompatActivity {
         });
 
         mRequestQueue.add(mStringRequest);
-
     }
 
 
     public void getLocation() {
-
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
