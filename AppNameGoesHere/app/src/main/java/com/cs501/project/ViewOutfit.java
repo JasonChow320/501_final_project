254735d1bfaef9e0d285fdc15eb3370ad385cb54
Jason Chow <jchow320@bu.edu>
2022-12-05 12:09:18 -0500

    add firebase rules and make list adapter for outfits

--- AppNameGoesHere/app/src/main/java/com/cs501/project/ViewOutfit.java
+++ AppNameGoesHere/app/src/main/java/com/cs501/project/ViewOutfit.java
@@ -12,6 +12,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,8 +26,18 @@ import android.widget.Toast;
 
 import com.cs501.project.Model.Clothes;
 import com.cs501.project.Model.FireBaseManager;
+import com.cs501.project.Model.Outfit;
+import com.cs501.project.Model.Profile;
+import com.cs501.project.Model.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -50,26 +61,65 @@ public class ViewOutfit extends AppCompatActivity {
         noneThereOutfit = (TextView) findViewById(R.id.noneThereOutfit);
         noneThereOutfit.setVisibility(View.GONE);
 
+        fb_manager = FireBaseManager.getInstance();
 
+        // Get account's users
+        //fb_manager = FireBaseManager.getInstance();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+        // retrieve account database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("accounts");
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        Log.d(TAG, "Signed in as user: " + currentUser.getUid());
+
+        // Initialize our firebase manager
+        fb_manager = FireBaseManager.getInstance();
+
+        // Have our listview auto update when database changes
+        myRef.child(currentUser.getUid()).child("users").child(String.valueOf(fb_manager.getUserIdx())).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Log.d(TAG, "Got data from database");
+                User user = dataSnapshot.getValue(User.class);
+
+                // update list
+                lvOutfits = (ListView)findViewById(R.id.lvOutfits);
+
+                lvAdapter = new ViewOutfitAdapter(ViewOutfit.this, user.getWardrobe().getOutfits());  //instead of passing the boring default string adapter, let's pass our own, see class MyCustomAdapter below!
+                lvOutfits.setAdapter(lvAdapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
 }
 
 // custom adapter
 class ViewOutfitAdapter extends BaseAdapter {
 
-    ArrayList<Clothes> clothes;
+    ArrayList<Outfit> outfits;
     SharedPreferences sharedPreferences;
     Context context;
     ViewOutfitAdapter adapter;
 
-    public ViewOutfitAdapter(Context aContext, ArrayList<Clothes> clothes) {
+    public ViewOutfitAdapter(Context aContext, ArrayList<Outfit> outfits) {
         //initializing our data in the constructor.
         context = aContext;
 
-        if(clothes == null || clothes.size() <= 0){
-            this.clothes = new ArrayList<Clothes>();
+        if(outfits == null || outfits.size() <= 0){
+            this.outfits = new ArrayList<Outfit>();
         } else{
-            this.clothes = clothes;
+            this.outfits = outfits;
         }
         this.adapter=this;
         sharedPreferences = context.getSharedPreferences("MySharedPref", MODE_PRIVATE);
@@ -77,12 +127,12 @@ class ViewOutfitAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return clothes.size();
+        return outfits.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return clothes.get(position);
+        return outfits.get(position);
     }
 
     @Override
@@ -99,82 +149,6 @@ class ViewOutfitAdapter extends BaseAdapter {
         } else {
             row = convertView;
         }
-
-        TextView clothes_type = (TextView) row.findViewById(R.id.clothes_type_view);
-        TextView clothes_id = (TextView) row.findViewById(R.id.clothes_id_view);
-        ImageView image = (ImageView) row.findViewById(R.id.clothes_image);
-        View color1 = (View) row.findViewById(R.id.color1);
-        View color2 = (View) row.findViewById(R.id.color2);
-        Button edit = (Button) row.findViewById(R.id.edit_button);
-        Button delete = (Button) row.findViewById(R.id.delete_button);
-        ImageView waterIndicator = (ImageView) row.findViewById(R.id.waterproofIndicator);
-
-        Clothes clothes_view = clothes.get(position);
-
-        if(clothes_view.isWaterResistant()) {
-            waterIndicator.setVisibility(View.VISIBLE);
-        } else {
-            waterIndicator.setVisibility(View.INVISIBLE);
-        }
-
-        // everything we add to view should be in the try catch
-        try {
-            clothes_type.setText(clothes_view.getType().name());
-            clothes_id.setText(clothes_view.getUniqueId());
-        } catch (Exception e){
-            Toast.makeText(context, "Unable to parse data for the " + position + " clothing. Please try again",
-                    Toast.LENGTH_SHORT).show();
-        }
-
-        try {
-            String hex1 = clothes_view.getColor().getHex1();
-            String hex2 = clothes_view.getColor().getHex2();
-
-            color1.setBackgroundColor(Color.parseColor(hex1));
-            color2.setBackgroundColor(Color.parseColor(hex2));
-        } catch (Exception e) {
-            Toast.makeText(context, "Unable to parse color data for the " + position + " clothing.",
-                    Toast.LENGTH_SHORT).show();
-        }
-
-        // get image
-        StorageReference pathReference = FirebaseStorage.getInstance().getReference();
-        pathReference.child(clothes_view.getImageURL()).getBytes(ConfirmToWardrobe.MAX_IMAGE_SIZE).addOnCompleteListener(new OnCompleteListener<byte[]>() {
-            @Override
-            public void onComplete(@NonNull Task<byte[]> task) {
-                try{
-                    byte[] bytes = task.getResult();
-                    Bitmap b = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-                    image.setImageBitmap(b);
-                } catch (Exception e){
-                    Toast.makeText(context, "Unable to parse image for the " + position + " clothing. Please try again",
-                            Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        SharedPreferences sh = context.getSharedPreferences("MySharedPref", MODE_PRIVATE);
-
-        delete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //clothes.remove(position);
-                FireBaseManager.getInstance().deleteItem(clothes_view.getUniqueId());
-                Toast.makeText(context, "Deleted item " + position + ".",
-                        Toast.LENGTH_SHORT).show();
-                //adapter.notifyDataSetChanged();
-            }
-        });
-
-        edit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(context, ConfirmToWardrobe.class);
-                i.putExtra("itemId", clothes_view.getUniqueId());
-                context.startActivity(i);
-                //adapter.notifyDataSetChanged();
-            }
-        });
 
         return row;
     }
