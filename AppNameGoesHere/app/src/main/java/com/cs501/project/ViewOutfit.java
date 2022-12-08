92c91dd080d0baec415896e8fb95d9a8620cf08f
revelationsz <rhinoam@gmail.com>
2022-12-08 17:35:48 -0500

    added darkmode on outfit view

--- AppNameGoesHere/app/src/main/java/com/cs501/project/ViewOutfit.java
+++ AppNameGoesHere/app/src/main/java/com/cs501/project/ViewOutfit.java
@@ -4,6 +4,7 @@ import static android.content.Context.MODE_PRIVATE;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Context;
 import android.content.Intent;
@@ -32,6 +33,7 @@ import com.cs501.project.Model.FireBaseManager;
 import com.cs501.project.Model.Outfit;
 import com.cs501.project.Model.Profile;
 import com.cs501.project.Model.User;
+import com.cs501.project.Model.User_settings;
 import com.cs501.project.Model.Wardrobe;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -111,6 +113,16 @@ public class ViewOutfit extends AppCompatActivity {
             }
         });
     }
+
+    public void onStart() {
+        super.onStart();
+        User_settings uSettings = fb_manager.getUser().getUserSettings();
+        ConstraintLayout con = findViewById(R.id.background);
+        String backgroundColor = getResources().getStringArray(R.array.themesValues)[uSettings.getTheme()];
+        System.out.println(backgroundColor);
+        con.setBackgroundColor(Color.parseColor(backgroundColor));
+    }
+
 }
 
 // custom adapter
